f9169fe14d1c9fc7c10d392295e50cfbb4a68348
Christopher Okokon Ntuk <engchris95@gmail.com>
2021-08-25 16:52:07 +0100

    Periodically remove expired items from the database

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
+import { setInterval } from 'timers';
 import productRoutes from './routes/product'
+import { removeExpiredItems } from './functions/index'
 
 const router = express();
 
@@ -8,6 +10,11 @@ router.use(express.json());
 
 router.use("/", productRoutes);
 router.get("*", (req: Request, res: Response) => res.redirect("/"));
+
+// Clear expired items from the database every 5 minute
+setInterval(() => {
+    removeExpiredItems()
+}, 300000) // 1 minute = 60000, 5 minutes = (60000 * 5) = 300000
 
 const PORT: string = process.env.PORT || "3000"
 
--- src/functions/index.ts
+++ src/functions/index.ts
@@ -32,12 +32,41 @@ export const reduceSales = (results: any[], quantity: number) => {
     return true;
 }
 
+export const removeExpiredItems = async () => {
+    try {
+        let query = `SELECT id FROM items WHERE expiry < "${new Date().getTime()}"`;
+        const response = await QueryDB(query);
+        const results: any = response.results
+        if (results.length > 0) {
+            results.forEach((element: any) => {
+                deleteItemInDB(element.id)
+            });
+            console.log("Expired items has been cleared")
+        } else {
+            console.log("No Expired items 😘")
+        }
+    } catch (error: any) {
+        console.log("Error updating stock")
+        console.log(error.message)
+    }
+}
+
 const updateDB = (item: { id: number, quantity: number }) => {
     try {
         let query = `UPDATE items SET quantity = ? WHERE id = ?`;
         QueryDB(query, [`${item.quantity}`, `${item.id}`]);
     } catch (error: any) {
         console.log("Error updating stock")
+        console.log(error.message)
+    }
+}
+
+const deleteItemInDB = (id: number) => {
+    try {
+        let query = `DELETE FROM items WHERE id = ?`;
+        QueryDB(query, [`${id}`]);
+    } catch (error: any) {
+        console.log("Error deleting item")
         console.log(error.message)
     }
 }
